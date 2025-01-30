b649e68592de0846c73ed85785a9e2bd3b1962e1
ThaungThanHanLamduan <6631503091@lamduan.mfu.ac.th>
2025-01-30 11:13:13 +0700

    feat: setup repository pattern

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  final String baseUrl = 'https://www.themealdb.com/api/json/v1/1'; // replace with backend URL
+
+  Future<dynamic> get(String endpoint) async {
+    final response = await http.get(Uri.parse('$baseUrl$endpoint'));
+
+    if(response.statusCode == 200){
+      return jsonDecode(response.body);
+    }else {
+      throw Exception('Failed to fetch data. Status Code: ${response.statusCode}');
+    }
+  }
+}
